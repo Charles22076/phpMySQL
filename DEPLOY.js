fffe08b432813a4c4e72aecbbcd04ad85e874d2f
Charles22076 <113404266+Charles22076@users.noreply.github.com>
2022-09-13 02:24:28 -0600

    $ firebase deploy

---
+++ DEPLOY.js
@@ -0,0 +1,24 @@
+Windows Registry Editor Version 5.00 [HKEY_CLASSES_ROOT\CLSID\{40419485-C444-4567-851A-2DD7BFA1684D}] @="Phone and Modem Control Panel" "InfoTip"=hex(2):40,00,25,00,53,00,79,00,73,00,74,00,65,00,6d,00,52,00,6f,00,\ 6f,00,74,00,25,00,5c,00,53,00,79,00,73,00,74,00,65,00,6d,00,33,00,32,00,5c,\ 00,74,00,65,00,6c,00,65,00,70,00,68,00,6f,00,6e,00,2e,00,63,00,70,00,6c,00,\ 2c,00,2d,00,32,00,23,00,69,00,6d,00,6d,00,75,00,74,00,61,00,62,00,6c,00,65,\ 00,31,00,00,00 "LocalizedString"=hex(2):40,00,25,00,53,00,79,00,73,00,74,00,65,00,6d,00,52,00,\ 6f,00,6f,00,74,00,25,00,5c,00,53,00,79,00,73,00,74,00,65,00,6d,00,33,00,32,\ 00,5c,00,74,00,65,00,6c,00,65,00,70,00,68,00,6f,00,6e,00,2e,00,63,00,70,00,\ 6c,00,2c,00,2d,00,31,00,23,00,69,00,6d,00,6d,00,75,00,74,00,61,00,62,00,6c,\ 00,65,00,31,00,00,00 "System.ApplicationName"="Microsoft.PhoneAndModem" "System.ControlPanel.Category"=dword:00000000 "System.Software.TasksFileUrl"="Internal" [HKEY_CLASSES_ROOT\CLSID\{40419485-C444-4567-851A-2DD7BFA1684D}\DefaultIcon] @=hex(2):25,00,53,00,79,00,73,00,74,00,65,00,6d,00,52,00,6f,00,6f,00,74,00,25,\ 00,5c,00,53,00,79,00,73,00,74,00,65,00,6d,00,33,00,32,00,5c,00,74,00,65,00,\ 6c,00,65,00,70,00,68,00,6f,00,6e,00,2e,00,63,00,70,00,6c,00,2c,00,2d,00,31,\ 00,30,00,30,00,00,00 [HKEY_CLASSES_ROOT\CLSID\{40419485-C444-4567-851A-2DD7BFA1684D}\Shell] [HKEY_CLASSES_ROOT\CLSID\{40419485-C444-4567-851A-2DD7BFA1684D}\Shell\Open] [HKEY_CLASSES_ROOT\CLSID\{40419485-C444-4567-851A-2DD7BFA1684D}\Shell\Open\Command] @=hex(2):25,00,53,00,79,00,73,00,74,00,65,00,6d,00,52,00,6f,00,6f,00,74,00,25,\ 00,5c,00,53,00,79,00,73,00,74,00,65,00,6d,00,33,00,32,00,5c,00,72,00,75,00,\ 6e,00,64,00,6c,00,6c,00,33,00,32,00,2e,00,65,00,78,00,65,00,20,00,25,00,53,\ 00,79,00,73,00,74,00,65,00,6d,00,52,00,6f,00,6f,00,74,00,25,00,5c,00,53,00,\ 79,00,73,00,74,00,65,00,6d,00,33,00,32,00,5c,00,73,00,68,00,65,00,6c,00,6c,\ 00,33,00,32,00,2e,00,64,00,6c,00,6c,00,2c,00,43,00,6f,00,6e,00,74,00,72,00,\ 6f,00,6c,00,5f,00,52,00,75,00,6e,00,44,00,4c,00,4c,00,20,00,25,00,53,00,79,\ 00,73,00,74,00,65,00,6d,00,52,00,6f,00,6f,00,74,00,25,00,5c,00,53,00,79,00,\ 73,00,74,00,65,00,6d,00,33,00,32,00,5c,00,74,00,65,00,6c,00,65,00,70,00,68,\ 00,6f,00,6e,00,2e,00,63,00,70,00,6c,00,00,00 [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel\NameSpace\{40419485-C444-4567-851A-2DD7BFA1684D}] @="Phone and Modem"
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBcY81OtADuOILwqJ7a2J6mgaBjCgNEc4w",
+    authDomain: "sms-gateway-app-3470b.firebaseapp.com",
+    projectId: "sms-gateway-app-3470b",
+    storageBucket: "sms-gateway-app-3470b.appspot.com",
+    messagingSenderId: "995424138239",
+    appId: "1:995424138239:web:e5cb8bd46352c3ab4e6dca",
+    measurementId: "G-ZGYYNHMBBW"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
